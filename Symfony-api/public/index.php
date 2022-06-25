af5234eda387e9347a8aa808f04d477043bc2c2c
Ait Benaissa Mohamed Hamza <mohamed-hamza.ait-benaissa@etu.univ-orleans.fr>
2022-06-25 03:23:35 +0200

    index.php

--- Symfony-api/public/index.php
+++ Symfony-api/public/index.php
@@ -5,7 +5,7 @@ use App\Kernel;
 require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    header('Access-Control-Allow-Origin: http://localhost:8101');
+    header('Access-Control-Allow-Origin: http://localhost:8100');
     header('Access-Control-Allow-Credentials: true');
     header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Origin, access-control-allow-credentials");
     header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
